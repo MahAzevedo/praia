6a84e4f6956562c6c614818b0ef416aee865ecf1
MahAzevedo <madavetto@gmail.com>
2025-02-02 18:16:27 -0300

    modified:   package-lock.json & package.json & praia/src/App.jsx

--- praia/src/App.jsx
+++ praia/src/App.jsx
@@ -9,34 +9,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-        </Routes>
+        <Header />
+        <main className="AppBody">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+          </Routes>
+        </main>
         <Footer />
-      </BrowserRouter>
-
-
-
-
-
-
-      
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      </BrowserRouter> 
     </div>
   );
 }
@@ -51,4 +31,21 @@ corpo - foto do lugar , sections
 
 footer - dados com endereço, telefone, instagram, tipo de pagamento, mapa, coisas assim
 
-*/ 
+*/
+
+/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> 
+*/
+
