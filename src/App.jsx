9d8264ca5ffa027ba4f413cfd81e65e1175605b2
MahAzevedo <madavetto@gmail.com>
2025-02-07 00:44:33 -0300

    new file:   src/assets/logo.jpeg

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+
 // import reactLogo from './assets/react.svg'; exemplo de imagem 
 // import viteLogo from '/vite.svg';
 
@@ -12,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Header />
           <Routes>
-            {/* <Route path="/" element={<Home />}/> */}
+            <Route path="/" element={<Home />}/>
           </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+
+
+// import Logo from '../Assets/logo.jpeg?react';
+// import Logo from '../Assets/react.svg';
+import Logo from '../Assets/logo.jpeg';
+// import reactLogo from './assets/react.svg';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Praia</h1>
-      <p>Hospedagem</p>
-    </div>
+    <header className={styles.header}>
+        <nav className={styles.nav}>
+            {/* <Link className={styles.logo} to="/" aria-label="Praia"> */}
+            {/* <Logo /> */}
+            {/* <img src={Logo} alt="Logo da Praia" /> */}
+            <Link className={styles.logo} to="/" aria-label="Praia">
+                <img src={Logo} alt="Logo da Praia" />
+            </Link>
+            {/* </Link> */}  
+          <h1>Praia</h1>
+          <p>Hospedagem</p>  
+        </nav>
+    </header>
   );
 };
 
