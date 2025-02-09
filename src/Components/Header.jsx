b3db0c4ae9db82879c5fc7a40a1189fb33a9ebed
MahAzevedo <madavetto@gmail.com>
2025-02-09 00:48:39 -0300

    modified:   src/Components/Header.jsx   &    src/Components/Header.module.css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Logo from '../Assets/logo.jpeg';
 
 const Header = () => {
   return (
     <header className={styles.header}>
         <nav className={styles.nav}>
-            <Link className={styles.logo} to="/" aria-label="Praia"> 
+            {/* <Link className={styles.logo} to="/" aria-label="Praia"> 
                 <Logo />
-            </Link>  
+            </Link>   */}
+            <NavLink>
+              <img src={Logo} alt="Logo da Praia" />
+            </NavLink>
           <h1>Praia</h1>
           <p>Hospedagem</p>  
         </nav>
