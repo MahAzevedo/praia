f219db8837821a6f93117e4fc9b5c95ca832ed2a
MahAzevedo <madavetto@gmail.com>
2025-02-11 19:20:26 -0300

    modified:   src/App.css & src/Components/Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className={Styles.footer}>
-      <div>
-        <p>Instagram</p>
-        <p>Contato: 9999-9999</p>
-      </div>
+    <footer className={styles.footer}>
+      <p>Praia - Site para hospedagem.</p>
+      <p>Instagram</p>
+      <p>Contato: 9999-9999</p>
+      <img src="/Assets/img-instagram.jpeg" alt="Logo do Instagram" /> 
     </footer>
   );
 };
