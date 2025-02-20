0b8c5152792a206c5ccdfbb7336f905e177eb59f
MahAzevedo <madavetto@gmail.com>
2025-02-20 01:47:41 -0300

    modified:   src/Components/Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import styles from './Banner.module.css';
 // import PhotoCapa from '../Assets/fotoCapaThayanaAlvarenga.jpg?react';
 import PraiaGrande from '../Assets/praia-grande.webp?react';
+import LogoMarca from '../Assets/logo.png?react';
 
 const Banner = () => {
   return (
     <div>
-        {/* <img className={styles.photoCapa} src={PhotoCapa} alt="Foto de Arraial de Cabo" /> */}
-        <img className={styles.photoCapa1} src={PraiaGrande} alt="Foto de Praia Grande"/>
+      <img className={styles.photoLogo} src={LogoMarca} alt="Foto da Logo"/>
     </div>
+    <div>
+      <img className={styles.photoPraia} src={PraiaGrande} alt="Foto de Praia Grande"/>
+    </div>
+    
   );
 };
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,48 +2,53 @@ import React from 'react';
 import styles from './Header.module.css';
 import { Link, NavLink } from 'react-router-dom';
 import Banner from './Banner';
+import LogoMarca from '../Assets/logo.png?react';
 // import Logo from '../Assets/logo.jpeg?react'; 
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Banner />
-      <NavLink to="/" className={styles.logo}>
-      
-        <h1>Renata Bahiense</h1>
-        <h2>aluguel de casa para temporada</h2>
-      </NavLink>
-      
         <nav className={styles.nav}>
-          <ul className={styles.headerUl}>
-            <li className={styles.headerUl}>
-              <NavLink to="/" className={styles.headerUl}>
+          <ul className={styles.nav}>
+            <li className={styles.nav}>
+            <img className={styles.photoLogo} src={LogoMarca} alt="Foto da Logo"/>
+              <NavLink to="/" className={styles.logo}>
+                <h1 className={styles.logo}>
+                   - Renata Bahiense - 
+                </h1>
+                <h2 className={styles.logo}>
+                   - aluguel de casa para temporada - 
+                </h2>
+              </NavLink>
+            </li>
+            <li className={styles.nav}>
+              <NavLink to="/" className={styles.nav}>
                 Casa
               </NavLink>
             </li>
-            <li className={styles.headerUl}>
-              <NavLink to="/" className={styles.headerUl}>
+            <li className={styles.nav}>
+              <NavLink to="/" className={styles.nav}>
                 Sobre
               </NavLink>
             </li>
-            <li className={styles.headerUl}>
-              <NavLink to="/" className={styles.headerUl}>
+            <li className={styles.nav}>
+              <NavLink to="/" className={styles.nav}>
                 Atrações turísticas
               </NavLink>
             </li>
-            <li className={styles.headerUl}>
-              <NavLink to="/" className={styles.headerUl}>
+            <li className={styles.nav}>
+              <NavLink to="/" className={styles.nav}>
                 Mapa
               </NavLink>
             </li>
-            <li className={styles.headerUl}>
-              <NavLink to="/" className={styles.headerUl}>
+            <li className={styles.nav}>
+              <NavLink to="/" className={styles.nav}>
                 Contato
               </NavLink>
             </li>
           </ul> 
         </nav>
-      {/* <Banner /> */}
+      <Banner />
     </header>
   );
 };
