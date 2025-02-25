360b4c3bb265f76c4999f12105fb6f8f23e744bf
MahAzevedo <madavetto@gmail.com>
2025-02-25 19:49:59 -0300

    modified:   src/Components/Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,45 +1,48 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link, NavLink } from 'react-router-dom';
-// import Logo from '../Assets/logo.jpeg?react'; 
 
 const Header = () => {
   return (
     <header className={styles.header}>
-        <nav className={styles.nav}>
-          <ul className={styles.nav}>
-            <li className={styles.nav}>
-              <NavLink to="/" className={styles.logo}>
-                <h1 className={styles.logo}>
+      <div>
+        <ul>
+        <li className={styles}>
+              <NavLink to="/" className={styles}>
+                <h1 className={styles}>
                    - Renata Bahiense - 
                 </h1>
-                <h2 className={styles.logo}>
+                <h2 className={styles}>
                    - aluguel de casa para temporada - 
                 </h2>
               </NavLink>
             </li>
-            <li className={styles.nav}>
-              <NavLink to="/" className={styles.nav}>
+        </ul>
+      </div>
+        <nav className={styles.nav}>
+          <ul className={styles.navUl}>
+            <li className={styles}>
+              <NavLink to="/" className={styles}>
                 Casa
               </NavLink>
             </li>
-            <li className={styles.nav}>
-              <NavLink to="/" className={styles.nav}>
+            <li className={styles.navMenu}>
+              <NavLink to="/" className={styles}>
                 Sobre
               </NavLink>
             </li>
-            <li className={styles.nav}>
-              <NavLink to="/" className={styles.nav}>
-                Atrações turísticas
+            <li className={styles}>
+              <NavLink to="/" className={styles}>
+                Turismo
               </NavLink>
             </li>
-            <li className={styles.nav}>
-              <NavLink to="/" className={styles.nav}>
+            <li className={styles}>
+              <NavLink to="/" className={styles}>
                 Mapa
               </NavLink>
             </li>
-            <li className={styles.nav}>
-              <NavLink to="/" className={styles.nav}>
+            <li className={styles}>
+              <NavLink to="/" className={styles}>
                 Contato
               </NavLink>
             </li>
