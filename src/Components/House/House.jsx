71a1b3abefda14e7b841430e7bcac6be468e8618
MahAzevedo <madavetto@gmail.com>
2025-02-27 00:25:29 -0300

    modified:   src/Components/Footer.module.css

--- src/Components/House/House.jsx
+++ src/Components/House/House.jsx
@@ -6,7 +6,6 @@ const House = () => {
     <section className={styles.containerHouse}>
       <h1 className={styles.houseH1}> - Casa - Fotos & Vídeos - </h1>
       <ul className={styles.houseSlider}>
-        <li>Casa - Fotos & Vídeos</li>
         <li>Aqui entra um COMPONENTE DE SLIDER carrossel</li>
       </ul>
     </section>
