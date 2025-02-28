bf5ebc0f4d33920821e267e5bc7c470abe17d200
MahAzevedo <madavetto@gmail.com>
2025-02-28 01:57:11 -0300

    modified:   src/Components/Footer.jsx  &  src/Components/Footer.module.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './Footer.module.css';
-// import Logo from '../Assets/logo.jpeg?react'; funcionando ok
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <p>Praia - Site para hospedagem.</p>
+    <footer className={styles.containerFooter}>
+      <div className={styles.footer}></div>
+      <p>Renata Bahiense - aluguel de casa para hospedagem.</p>
       <p>Instagram</p>
       <p>Contato: 9999-9999</p>
-      {/* <img src={Logo} alt="Logo do Instagram" />  */}
     </footer>
   );
 };
