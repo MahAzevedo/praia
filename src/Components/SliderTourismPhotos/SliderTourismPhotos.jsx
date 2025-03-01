91381c5a9c2de905c6ea09f74659efa3539520a8
MahAzevedo <madavetto@gmail.com>
2025-03-01 18:12:10 -0300

    new file: src/Components/SliderTourismPhotos/SliderTourismPhotos.jsx

---
+++ src/Components/SliderTourismPhotos/SliderTourismPhotos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './SliderTourismPhotos.module.css';
+
+const SliderTourismPhotos = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default SliderTourismPhotos;
