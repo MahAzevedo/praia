d74db813d26f1d82f0d423554f2fe0f6f67787c9
MahAzevedo <madavetto@gmail.com>
2025-03-05 20:41:51 -0300

    modified:   src/Components/SliderTourismPhotos/SliderTourismPhotos.jsx

--- src/Components/SliderTourismPhotos/SliderTourismPhotos.jsx
+++ src/Components/SliderTourismPhotos/SliderTourismPhotos.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import styles from './SliderTourismPhotos.module.css';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import Carousel from 'react-bootstrap/Carousel';
-// import FotoCidade from '../Assets/fotoCidadeArraial.jpg?react';
-// import PraiaGrande from './praia-grande.webp?react';
 
 const SliderTourismPhotos = () => {
     return (
