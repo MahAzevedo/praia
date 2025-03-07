567be7ba5a00cf00365f7f995bbf2b7bea1dfa0f
MahAzevedo <madavetto@gmail.com>
2025-03-07 17:53:01 -0300

    deleted:    src/Components/SliderTourismPhotos/SliderTourismPhotos.jsx

--- src/Components/House/House.jsx
+++ src/Components/House/House.jsx
@@ -13,3 +13,29 @@ const House = () => {
 };
 
 export default House;
+
+{/* <div id="carouselControls" className={'carousel slide'} data-bs-ride="carousel">
+      <div className={'carousel-inner'}>
+        <div className={'carousel-item active'}>
+          <img src="./Assets/arraial3.jpg" className="d-block w-100" alt="Imagem de Arraial..." />
+        </div>
+        <div className={'carousel-item'}>
+          <img src="..." className={'d-block w-100'} alt="..." />
+        </div>
+        <div className={'carousel-item'}>
+          <img src="..." className={'d-block w-100'} alt="..." />
+        </div>
+      </div>
+      <button className={'carousel-control-prev'} type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+        <span className={'carousel-control-prev-icon'} aria-hidden="true"></span>
+        <span className={'visually-hidden'}>
+          Previous
+        </span>
+      </button>
+      <button className={'carousel-control-next'} type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+        <span className={'carousel-control-next-icon'} aria-hidden="true"></span>
+        <span className={'visually-hidden'}>
+          Next
+        </span>
+      </button>
+    </div> */}
--- src/Components/Tourism/Tourism.jsx
+++ src/Components/Tourism/Tourism.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from './Tourism.module.css';
-import SliderTourismPhotos from '../SliderTourismPhotos/SliderTourismPhotos';
 
 const Tourism = () => {
   return (
     <section className={styles.containerTourism}>
-        <h1 className={styles.containerTourismH1}>
-            - Turismo - 
-        </h1>
-        <SliderTourismPhotos />
+      <h1 className={styles.containerTourismH1}>
+        - Turismo - 
+      </h1>
     </section>
   );
 };
