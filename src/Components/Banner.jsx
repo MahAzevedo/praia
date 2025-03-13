f7611d938a1b4c0084d6e20d012031c0bca747d7
MahAzevedo <madavetto@gmail.com>
2025-03-13 11:54:50 -0300

    modified:   src/Components/Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Banner.module.css';
 import PraiaGrande from '../Assets/praia-grande.webp?react';
 
-
 const Banner = () => {
   return (
     <img className={styles.photoPraia} src={PraiaGrande} alt="Foto de Praia Grande"/>
