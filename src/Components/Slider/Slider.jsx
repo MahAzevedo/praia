d990ea8505f373b2d1870fc77931135c5abfe561
MahAzevedo <madavetto@gmail.com>
2025-03-15 03:05:33 -0300

    modified:   src/Components/Slider/Slider.jsx

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Slider = () => {
     const data = [
-        {id: '1', image: ''},
+        {id: '1', image: './arraial3.jpg?react'},
         {id: '2', image: ''},
         {id: '3', image: ''},
         {id: '4', image: ''},
@@ -15,9 +15,16 @@ const Slider = () => {
       <h1 className={styles.title}>Slider com React JS</h1>
 
       <Swiper>
-        <SwiperSlide>
-
-        </SwiperSlide>
+        {data.map( (item) => (
+            <SwiperSlide key={item.id}>
+                <img 
+                    src={item.image} 
+                    alt="Slider"
+                    className={styles.slideItem} 
+                />
+                <img src="" alt="" />
+            </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   )
