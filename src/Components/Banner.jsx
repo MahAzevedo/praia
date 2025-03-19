bfd06188e99dc4ddf8c8462a31a3ef1aebbd2831
MahAzevedo <madavetto@gmail.com>
2025-03-18 21:42:29 -0300

    new file:   src/Components/Carousel/CarouselData.json

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,17 +7,35 @@ import praiaCapa from '../Assets/praiaCapa.jpg?react';
 const Banner = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.info}>
+        Contato Whatsapp 9999-9999 | e-mail:  blablabla@gmail.com
+      </div>
       <div className={styles.carousel}>
-        <div className={styles.item}>
-          <div className={styles.image}>
-            <img src="./src/Assets/praiaCapa.jpg" alt="Praia 1" />
-          </div>
-          <div className={styles.info}>
-            <span className={styles.msg}>Praia Arraial</span>
-          </div>
-        </div>
+        <img src="./src/Assets/praiaCapa.jpg" alt="Praia 1" />  
+      </div>
+      <span className={styles.msg}>Praia Arraial</span>
+      <div className={styles.info}>
       </div>
     </div>
+
+// https://unsplash.com/pt-br/fotografias/mar-azul-sob-o-ceu-azul-durante-o-dia--awuLy3I56c
+
+//https://unsplash.com/pt-br/fotografias/uma-vista-aerea-de-uma-praia-e-do-oceano-M50irPDj9Kc
+
+
+
+    // <div className={styles.container}>
+    //   <div className={styles.carousel}>
+    //     <div className={styles.item}>
+    //       <div className={styles.image}>
+    //         <img src="./src/Assets/praiaCapa.jpg" alt="Praia 1" />
+    //       </div>
+    //       <div className={styles.info}>
+    //         <span className={styles.msg}>Praia Arraial</span>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
