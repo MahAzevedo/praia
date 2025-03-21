1f8b5ff3af0d81278b95c54d7a447d8b759236c4
MahAzevedo <madavetto@gmail.com>
2025-03-21 00:53:39 -0300

    modified:   src/Components/TesteCarousel/TesteCarousel.jsx

--- src/Components/TesteCarousel/TesteCarousel.jsx
+++ src/Components/TesteCarousel/TesteCarousel.jsx
@@ -3,17 +3,72 @@ import styles from './TesteCarousel.module.css';
 
 const TesteCarousel = () => {
   return (
-    <div className={styles.carousel}>
-        <div className={styles.item}>
-            <div className={styles.image}>
-                <img src="/" alt="Praia" />
-            </div>
-        </div>
-    </div>
+   <div>
+    <p>Carousel linkado</p>
+   </div>
   );
 };
 
 export default TesteCarousel;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect } from 'react';
+// import styles from './TesteCarousel.module.css';
+
+// const TesteCarousel = () => {
+//     useEffect(() => {
+//         fetch('') 
+//     }, []);
+
+//   return (
+//     <div className={styles.carousel}>
+//         <div className={styles.item}>
+//             <div className={styles.image}>
+//                 <img src="/" alt="Praia" />
+//             </div>
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default TesteCarousel;
 
 // as fotos do carousel vão vir de uma API que vou criar e se localizão no ITEM
 
