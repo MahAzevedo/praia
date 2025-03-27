a809b8e877bc8178ac518dace310e5265b69598c
MahAzevedo <madavetto@gmail.com>
2025-03-27 00:20:56 -0300

    modified:   src/Components/CustomCarousel/CustomCarousel.module.css

---
+++ src/Components/SlickCarousel/SlickCarousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './SlickCarousel.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SlickCarousel = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default SlickCarousel;
